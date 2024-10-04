3970240b9bf089e8d80ed552fd9e942e01320a16
John Watson <john.watson1@defra.gov.uk>
2024-10-04 16:53:04 +0100

    Add initial calculation

---
+++ src/groups.js
@@ -0,0 +1,25 @@
+const groupA = {
+  name: 'Group A',
+  managers: [
+    'Billy',
+    'Lee',
+    'Darren',
+    'Scott/Ben',
+    'John',
+  ]
+}
+
+const groupB = {
+  name: 'Group B',
+  managers: [
+    'Rob',
+    'Bob',
+    'Michael',
+    'Dave',
+    'Tucker/Karl',
+  ]
+}
+
+const groups = [groupA, groupB]
+
+export default groups
---
+++ src/weeks.js
@@ -0,0 +1,14 @@
+const weeks = [
+  9,
+  11,
+  12,
+  14,
+  16,
+  18,
+  20,
+  27,
+  44,
+  45,
+]
+
+export default weeks
