4d9b158da1828e19efbb7177152245390d45946a
John Watson <john.watson1@defra.gov.uk>
2024-10-04 17:09:18 +0100

    Refactor

--- src/calculate.js
+++ src/calculate.js
@@ -6,8 +6,6 @@ const BYE = 'Bye'
 function calculateSchedule() {
   for (const group of groups) {
     console.log(`${ group.name } fixtures:`)
-    const initialFixtures = []
-
     let totalManagers = group.managers.length
 
     // if odd number of managers, add a BYE manager to make it even
@@ -16,29 +14,9 @@ function calculateSchedule() {
       totalManagers++
     }
 
-    const managerFixtures = calculateFixtures(totalManagers)
-
-    for (const manager of managerFixtures) {
-      for (let i = 0; i < manager.matches.length; i++) {
-        initialFixtures.push({ weekIndex: i, home: group.managers[manager.managerIndex], away: group.managers[manager.matches[i]] })
-      }
-    }
-
-    let confirmedFixtures = []
-
-    for (let i = 0; i < initialFixtures.length; i++) {
-      const conflictingFixture = confirmedFixtures.find(x => x.weekIndex === initialFixtures[i].weekIndex && (x.home === initialFixtures[i].home || x.away === initialFixtures[i].home || x.home === initialFixtures[i].away || x.away === initialFixtures[i].away))
-      if (conflictingFixture) {
-        if (i % 2 === 0) {
-          initialFixtures[i].weekIndex = initialFixtures[i].weekIndex + totalManagers - 1
-        } else {
-          conflictingFixture.weekIndex = conflictingFixture.weekIndex + totalManagers - 1
-          conflictingFixture.weekNumber = weeks[conflictingFixture.weekIndex]
-        }
-      }
-      initialFixtures[i].weekNumber = weeks[initialFixtures[i].weekIndex]
-      confirmedFixtures.push(initialFixtures[i])
-    }
+    const managerFixtures = calculateManagerFixtures(totalManagers)
+    const initialFixtures = calculateFixtureProperties(managerFixtures, group)
+    const confirmedFixtures = calculateFixtureWeeks(initialFixtures, totalManagers)
 
     confirmedFixtures.sort((a, b) => a.weekIndex - b.weekIndex)
 
@@ -49,23 +27,52 @@ function calculateSchedule() {
   }
 }
 
-function calculateFixtures(numberOfTeams) {
-  let rounds = Array.from({ length: numberOfTeams - 1 }, (_, j) => calculateTeamFixtures(numberOfTeams, j))
+function calculateManagerFixtures(numberOfTeams) {
+  let rounds = Array.from({ length: numberOfTeams - 1 }, (_, j) => calculateRound(numberOfTeams, j))
   return Array.from({ length: numberOfTeams }, (_, i) => ({
     managerIndex: i,
     matches: rounds.map(round => round[i])
   }))
 }
 
-function calculateTeamFixtures(numberOfTeams, j) {
+function calculateRound(numberOfTeams, j) {
   let m = numberOfTeams - 1
   let round = Array.from({ length: numberOfTeams }, (_, i) => (m + j - i) % m) // circular shift
-  round[round[m] = j * (numberOfTeams >> 1) % m] = m // swapping self-match
+  round[m] = j * (numberOfTeams >> 1) % m
+  round[round[m]] = m // swapping self-match
   return round
 }
 
+function calculateFixtureProperties(managerFixtures, group) {
+  const fixtures = []
+  for (const manager of managerFixtures) {
+    for (let i = 0; i < manager.matches.length; i++) {
+      fixtures.push({ weekIndex: i, home: group.managers[manager.managerIndex], away: group.managers[manager.matches[i]] })
+    }
+  }
+  return fixtures
+}
+
+function calculateFixtureWeeks(initialFixtures, totalManagers) {
+  const confirmedFixtures = []
+  for (let i = 0; i < initialFixtures.length; i++) {
+    const conflictingFixture = confirmedFixtures.find(x => x.weekIndex === initialFixtures[i].weekIndex && (x.home === initialFixtures[i].home || x.away === initialFixtures[i].home || x.home === initialFixtures[i].away || x.away === initialFixtures[i].away))
+    if (conflictingFixture) {
+      if (i % 2 === 0) {
+        initialFixtures[i].weekIndex = initialFixtures[i].weekIndex + totalManagers - 1
+      } else {
+        conflictingFixture.weekIndex = conflictingFixture.weekIndex + totalManagers - 1
+        conflictingFixture.weekNumber = weeks[conflictingFixture.weekIndex]
+      }
+    }
+    initialFixtures[i].weekNumber = weeks[initialFixtures[i].weekIndex]
+    confirmedFixtures.push(initialFixtures[i])
+  }
+  return confirmedFixtures
+}
+
 function getFixtureString(fixture) {
-  return `Week ${fixture.weekNumber}: ${ fixture.home } vs ${ fixture.away }`
+  return `Week ${ fixture.weekNumber }: ${ fixture.home } vs ${ fixture.away }`
 }
 
 export { calculateSchedule }
